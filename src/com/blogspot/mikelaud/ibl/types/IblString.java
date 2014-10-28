63980b6a3aa60cbbb5268680801edaf35a762326
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2014-10-28 12:36:21 +0400

    add to string

--- src/com/blogspot/mikelaud/ibl/types/IblString.java
+++ src/com/blogspot/mikelaud/ibl/types/IblString.java
@@ -1,13 +1,11 @@
 package com.blogspot.mikelaud.ibl.types;
 
+import java.util.Objects;
+
 public class IblString {
 
-	public static String nvl(String aString) {
-		return (null == aString ? "" : aString);
-	}
-	
 	public static String nvl(Object aArgument) {
-		return (null == aArgument ? "" : aArgument.toString());
+		return Objects.toString(aArgument, "");
 	}
-	
+		
 }
